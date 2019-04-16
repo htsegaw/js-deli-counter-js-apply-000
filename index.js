1359f0b937164f6b6775d8bf336009839708aa36
exuberant-minifier-2741 <htsegaw@gmail.com>
2019-04-16 18:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 
-function takeANumber(line, name){
-  line.push(name);
-  return `Welcome, ${name}. You are number ${line.length}.`;
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(name);
+  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
 }
 
-function nowServing(line){
-  var message = ''
-  if(line.length > 0){
-    message = `Now serving ${line.shift(line)}.`;
+
+
+function nowServing(katzDeliLine){
+
+  if(katzDeliLine.length === 0){
+    return 'There is nobody waiting to be served!';
   } else {
-    message = 'There is nobody waiting to be served!';
+    return 'Currently serving ' + katzDeliLine.shift() + '.';
   }
-  return message;
 }
 
 
-function currentLine(line){
-  var status = '';
-  if(line.length > 0){
-    for(var i = 0; i < line.length; i++){
-      status += `${i+1}. ${line[i]}`;
+function currentLine(katzDeliLine){
+    var line = '';
+    if(katzDeliLine.length === 0){
+      return 'The line is currently empty.';
+    } else {
+      for(var i = 0; i < katzDeliLine.length; i++){
+       line += (i+1) + '. ' + katzDeliLine[i] + ', ';
     }
-  } else {
-      status += 'empty.';
+    return "The line is currently: " + line.slice(0, line.length - 2);
   }
-  return `The line is currently:  ${status}`;
 }
