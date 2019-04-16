3f5bb47481858ec54d454d49316655c0bd7725e2
exuberant-minifier-2741 <htsegaw@gmail.com>
2019-04-16 18:02:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 function takeANumber(line, name){
   line.push(name);
-  return 'Welcome, ${name}. You are number ${line.length}.';
+  return `Welcome, ${name}. You are number ${line.length}.`;
 }
 
 function nowServing(line){
   var message = ''
   if(line.length > 0){
-    message = 'Now serving ${line.shift(line)}.';
+    message = `Now serving ${line.shift(line)}.`;
   } else {
     message = 'There is nobody waiting to be served!';
   }
@@ -19,10 +19,10 @@ function currentLine(line){
   var status = '';
   if(line.length > 0){
     for(var i = 0; i < line.length; i++){
-      status += '${i+1}. ${line[i]}';
+      status += `${i+1}. ${line[i]}`;
     }
   } else {
       status += 'empty.';
   }
-  return 'The line is currently:  ${status}';
+  return `The line is currently:  ${status}`;
 }
